3a32b460bc8cfa6a2e191441ca47ad25ea180ebf
lu-yanpeng <1102234236@qq.com>
2024-10-13 06:51:04 +0800

    遗漏文件

---
+++ frontend/muji-mall/src/pages/activity/activity.back.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+// @ts-ignore
+import Activity from 'remote-app/activity'
+
+const componentId = ref<string | null>(null)
+
+// @ts-ignore
+onLoad((query: {
+  title: string
+  'component-id': string
+}) => {
+  uni.setNavigationBarTitle({
+    title: query.title
+  })
+  componentId.value = query['component-id']
+})
+
+const mounted = () => {
+  console.log('挂载')
+}
+</script>
+
+<template>
+  <view>
+    <activity @mounted="mounted" v-if="componentId" :component-id="componentId" />
+  </view>
+</template>
